3df537c22ad35ba91201875a7680c050d561c7ba
Ishank2301 <ishankmishra579@gmail.com>
2026-01-13 20:55:04 +0530

    Resolved Security Issues

--- src/tmdb.py
+++ src/tmdb.py
@@ -1,7 +1,8 @@
 import requests
 import os
-
-API_KEY = "05da4598f508ce0195003479912d9bd6"
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://api.themoviedb.org/3"
 IMAGE_BASE = "https://image.tmdb.org/t/p/w500"
 
